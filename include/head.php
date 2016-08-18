2ebabc72e2db7d52869cc421409aefc820402059
Stacy Moorhead <spezzola@uarts.edu>
2016-08-18 20:41:29 +0000

    created includes for header and scripts, added nav

---
+++ include/head.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Stacy Lauren Designs</title>
+<link href="sass/style.css" rel="stylesheet">
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="gradient">
+    <div class="bg">
+        <nav class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <ul class="nav">
+                        <li><a href="">About</a></li>
+                        <li><a href="">Work</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 social">
+                    Testing 123
+                    <a href="http://www.facebook.com/stacy.pezzola"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="http://www.twitter.com/heartshapedfart"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    <a href="http://www.instagram.com/heartshapedfarts"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </div>    
+            </div>
+        </nav>
---
+++ include/scripts.php
@@ -0,0 +1,7 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.3.min.js"></script>
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>-->
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script>
+<script src="js/bucket.js"></script>
